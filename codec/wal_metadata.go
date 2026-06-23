110815156c4dbd503df4e36ca244e79622b6f40f
Adarsh-Kmt <adarshkmt2004@gmail.com>
2026-06-23 19:01:47 +0530

    Wrote codec for WAL metadata.

---
+++ codec/wal_metadata.go
@@ -0,0 +1,33 @@
+package codec
+
+import "encoding/binary"
+
+type WALMetadataCodec struct {
+	MetadataLength   int
+	currLSNOffset    int
+	checkpointOffset int
+}
+
+func NewWALMetadataCodec() WALMetadataCodec {
+
+	return WALMetadataCodec{
+		MetadataLength:   16,
+		currLSNOffset:    0,
+		checkpointOffset: 8,
+	}
+}
+
+func (codec WALMetadataCodec) EncodeWALMetadata(currLSN uint64, checkpointOffset uint64) []byte {
+
+	b := make([]byte, 0)
+
+	b = binary.LittleEndian.AppendUint64(b, currLSN)
+	b = binary.LittleEndian.AppendUint64(b, checkpointOffset)
+
+	return b
+}
+
+func (codec WALMetadataCodec) DecodeWALMetadata(metadataBytes []byte) (currLSN uint64, checkpointOffset uint64) {
+
+	return binary.LittleEndian.Uint64(metadataBytes[codec.currLSNOffset:]), binary.LittleEndian.Uint64(metadataBytes[codec.checkpointOffset:])
+}
